752ea4e42901f8d04ec33d9f13ed0be788fb518b
Afaq Mehmood <afaq.mehmood@uptek.com>
2024-08-20 23:46:38 +0500

    homepage except footer

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ import './style.css'
 // import Swiper bundle with all modules installed
 import Swiper from 'swiper/bundle';
 
-// Initialize Swiper
-const swiper = new Swiper('.swiper-container', {
+const swiperInitializer = ( selector,  options ) => {
+  const swiper = new Swiper(selector, options);
+  return swiper;
+}
+
+const heroSliderOptions = {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -21,9 +25,44 @@ const swiper = new Swiper('.swiper-container', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+}
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+const arrivals_Slider_Options = {
+  direction: 'horizontal',
+  loop: true,
+  crossFade: true,
+  speed: 700,
+  slidesPerView: 4,
+  spaceBetween: 38.58,
+  lazyloading: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
-});
+}
+const testimonials_Slider_Options = {
+  direction: 'horizontal',
+  loop: true,
+  crossFade: true,
+  speed: 700,
+  slidesPerView: 3,
+  spaceBetween: 23.19,
+  lazyloading: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+}
+
+const hero_selector = document.querySelector('.hero-slider');
+const arrivals_selector = document.querySelector('.arrival-cards-swiper');
+const testimonials_selector = document.querySelector('.testinomials-swiper');
+
+// Initialize Swiper
+const heroSlider = swiperInitializer(hero_selector, heroSliderOptions);
+const arrivalsSlider = swiperInitializer(arrivals_selector, arrivals_Slider_Options);
+const testimonialsSlider = swiperInitializer(testimonials_selector, testimonials_Slider_Options);
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ export default {
     "./**/*.{html,js}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '7.5': '1.875rem',
+        '12.5': '3.125rem',
+        '17.5': '4.375rem',
+        '18.5': '4.625rem',
+        '23.5': '5.875rem',
+      }
+    },
   },
   plugins: [],
 }
